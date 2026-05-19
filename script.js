4985aa87a3f3613c1fcf69f6279e30b737481189
unknown <binitbarui9090@gmail.com>
2026-05-19 15:51:26 +0530

    FIXED THE CODE

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let gender = prompt("Your gender");
 
 if (gender == "Male" || gender == "male") {
     alert("You are little gay");
-}else{
-    alert("nice try");
 }
 
 if (gender == "Female" || gender == "female") {
     alert("you are little lesbian")
+}else{
+    alert("nice try");
 }
